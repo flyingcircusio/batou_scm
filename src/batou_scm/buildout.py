49d95e7d18217ac8bb81e91baeb019036c453ed9
Thomas Lotze <tl@gocept.com>
2014-03-24 07:44:42 +0100

    added minimal buildout component that interacts with the source component

---
+++ src/batou_scm/buildout.py
@@ -0,0 +1,32 @@
+from batou.lib.buildout import Buildout
+from batou.lib.file import File
+import pkg_resources
+
+
+class Buildout(Buildout):
+
+    python = '2.7'
+    version = '2.2.0'
+    setuptools = '1.3'
+
+    def configure(self):
+        self.source = self.require_one('source', self.host)
+
+        self.dist_names, distributions = zip(
+            *sorted(self.source.distributions.items()))
+        self.dist_paths = [clone.target for clone in distributions]
+        self.additional_config += distributions
+
+        self.overrides = File(
+            'buildout_overrides.cfg',
+            source=pkg_resources.resource_filename(
+                'batou_scm', 'resources/buildout_overrides.cfg'))
+        self += self.overrides
+
+        super(Buildout, self).configure()
+
+    @property
+    def versions(self):
+        result = {'setuptools': self.setuptools, 'zc.buildout': self.version}
+        result.update({name: '' for name in self.dist_names})
+        return sorted(result.items())
---
+++ src/batou_scm/tests/test_buildout.py
@@ -0,0 +1,64 @@
+from batou.conftest import root
+from batou_scm.buildout import Buildout
+from batou_scm.source import Source
+import ConfigParser
+import StringIO
+import pytest
+
+
+@pytest.fixture
+def buildout(root):
+    root.component += Source(
+        dist_sources=repr([
+            'https://example.com/foo',
+            'https://example.com/bar',
+        ]))
+    root.component += Buildout()
+    root.component.configure()
+    return root.component._
+
+
+def read_config(content):
+    config = ConfigParser.ConfigParser()
+    config.readfp(StringIO.StringIO(content))
+    return config
+
+
+def test_buildout_depends_on_dist_clones(buildout):
+    dist_clones = buildout.source.distributions.values()
+    assert set(dist_clones).issubset(buildout.sub_components)
+
+
+def test_dist_paths_are_listed_as_develop_paths_in_overrides(buildout):
+    config = read_config(buildout.overrides.content)
+    assert set([
+        buildout.source.map('bar'),
+        buildout.source.map('foo'),
+    ]).issubset(config.get('buildout', 'develop +').split())
+
+
+def test_develop_paths_in_overrides_have_defined_order(buildout):
+    config = read_config(buildout.overrides.content)
+    develop_paths = config.get('buildout', 'develop +').split()
+    assert develop_paths == sorted(develop_paths)
+
+
+def test_dist_versions_are_unpinned_in_overrides(buildout):
+    config = read_config(buildout.overrides.content)
+    assert config.get('versions', 'foo') == ''
+    assert config.get('versions', 'bar') == ''
+
+
+def test_versions_of_setuptools_and_buildout_are_pinned_explicitly(buildout):
+    config = read_config(buildout.overrides.content)
+    assert config.get('versions', 'setuptools') == str(buildout.setuptools)
+    assert config.get('versions', 'zc.buildout') == str(buildout.version)
+
+
+def test_version_pins_in_overrides_have_defined_order(buildout):
+    config = read_config(buildout.overrides.content)
+    pins = config.items('versions')
+    assert pins == sorted(pins)
+
+
+root  # XXX satisfy pyflakes
