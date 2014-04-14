a7607ad225cfe16d4a1161f717fe68f29c3de97a
Thomas Lotze <tl@gocept.com>
2014-04-14 08:06:20 +0200

    made source component optional for buildouts (e.g., production might have no source at all)

--- src/batou_scm/tests/test_buildout.py
+++ src/batou_scm/tests/test_buildout.py
@@ -29,6 +29,14 @@ def read_config(content):
     return config
 
 
+def test_buildout_doesnt_need_source_to_be_configured(root):
+    root.component += Buildout()
+    root.component.configure()
+    assert '.batou-shared-eggs' in root.component._.overrides.content
+    with pytest.raises(UpdateNeeded):
+        root.component._.verify()
+
+
 def test_dist_paths_are_listed_as_develop_paths_in_overrides(buildout):
     config = read_config(buildout.overrides.content)
     assert set([
